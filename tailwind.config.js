1fdfd0e1877b42f702a1b407b197f557ac0dd614
basjosben <basjosben@gmail.com>
2023-06-23 21:15:50 -0700

    Created main component

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+export default {
+  important: true,
+  content: [
+    "./index.html",
+    "./app/javascript/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import resolve from './resolve';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  define: { 'process.env': {} },
+  resolve,
+  server: {
+    port: 3000,
+    open: true,
+  },
+  root: 'app/javascript/src',
+  clearScreen: false,
   plugins: [react()],
-})
+  babel: {
+    requireConfigFile: false,
+  },
+});
